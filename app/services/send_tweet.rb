c23095cc5a59607b30715be92d3ee9809ba63bfe
Arnaud AGBO <agbo.arnaud@yahoo.fr>
2017-11-09 21:59:43 +0100

    tweeeeet

---
+++ app/services/send_tweet.rb
@@ -0,0 +1,27 @@
+class SendTweet
+
+  def initialize(string)
+    @string = string
+  end
+
+
+
+  def log_in_to_twitter
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ""
+      config.consumer_secret     = ""
+      config.access_token        = ""
+      config.access_token_secret = ""
+    end
+  end
+
+   def perform
+     log_in_to_twitter
+    @client.update("@string")
+   end
+
+end
+
+
+
+
